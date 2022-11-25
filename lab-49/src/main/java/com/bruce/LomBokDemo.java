edbeda2992f211685d1d01a2f783591b19950f70
Bruce <wenjie.wang01@liulishuo.com>
2022-11-25 14:32:57 +0800

    lab-49 lombok demo.

---
+++ lab-49/src/main/java/com/bruce/LomBokDemo.java
@@ -0,0 +1,15 @@
+package com.bruce;
+
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author Bruce
+ */
+@Data
+@Slf4j
+public class LomBokDemo {
+    private String userName;
+    private Integer age;
+}
